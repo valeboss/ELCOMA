a30822f7cd907b304be448d8ba6bdf7f3fb28a12
valeboss <v.boss@gmx.de>
2016-09-18 19:50:27 +0200

    Renamed header_component to head_component. Added code for showing a head toolbar.

---
+++ lib/head_component.dart
@@ -0,0 +1,15 @@
+import 'package:angular2/core.dart';
+
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer_elements/paper_icon_button.dart';
+
+
+@Component(
+  selector: 'my-head',
+  encapsulation: ViewEncapsulation.Native,
+  templateUrl: 'templates/head_component.html'
+)
+class MyHeaderComponent {
+
+}
